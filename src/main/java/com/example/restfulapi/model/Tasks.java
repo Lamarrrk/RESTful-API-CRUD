d49c8af3d2984a2257ecb9b4c9acbf72553c0bb5
Lamarrrk <londonbridge1602@gmail.com>
2024-04-03 03:21:09 +0300

    Removed garbage code, implemented automatically created controllers using a plugin

--- src/main/java/com/example/restfulapi/model/Tasks.java
+++ src/main/java/com/example/restfulapi/model/Tasks.java
@@ -1,15 +1,10 @@
 package com.example.restfulapi.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.Objects;
-
-@NoArgsConstructor
-@AllArgsConstructor
 @Getter
 @Setter
 @Entity
@@ -29,27 +24,4 @@ public class Tasks {
     @Column(name = "status", nullable = false)
     private String status;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Tasks tasks = (Tasks) o;
-        return Objects.equals(id, tasks.id) && Objects.equals(title, tasks.title) && Objects.equals(description, tasks.description) && Objects.equals(status, tasks.status);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, title, description, status);
-    }
-
-    @Override
-    public String toString() {
-        return "tasks{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                ", status='" + status + '\'' +
-                '}';
-    }
 }
-
--- src/main/java/com/example/restfulapi/repo/TasksRepository.java
+++ src/main/java/com/example/restfulapi/repo/TasksRepository.java
@@ -2,7 +2,9 @@ package com.example.restfulapi.repo;
 
 import com.example.restfulapi.model.Tasks;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface TasksRepository extends JpaRepository <Tasks, Integer> {
-
+@RepositoryRestResource(collectionResourceRel = "tasks", path = "tasks")
+public interface TasksRepository extends JpaRepository<Tasks, Integer> {
 }
+
